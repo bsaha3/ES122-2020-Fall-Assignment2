9234d3bb9e92769489d9486def1120a030f2ca40
Biswa <saha.biswajit.1992@gmail.com>
2020-10-17 17:47:35 -0500

    Create assignment_2.py

---
+++ assignment_2.py
@@ -0,0 +1,7 @@
+#Please use different functions for each program.
+#Use comments.
+#Do not print anything else in the output other than what is mentioned.
+#Give the correct filename(as given, not the one that is on your local machine) while reading a file.
+#Plagiariasm will be checked this time.
+#Call every function after defining them, so that the funcion executes.
+#Before submission, make sure to run the program and check whether everything is working.
